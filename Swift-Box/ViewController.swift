b665963851172ac6bf7f04d717772cb4ec09c9da
vinner2112 <Ayaaz@Moxi4.local>
2014-08-17 19:28:50 +0530

    Initial Commit

---
+++ Swift-Box/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Box
+//
+//  Created by Moxi_Mac on 17/08/14.
+//  Copyright (c) 2014 ES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
